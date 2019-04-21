2ed353d209cb176c1844d21ae53eec198d1a0ba5
james-braun <braun.james.robert@gmail.com>
2019-04-20 22:37:43 -0500

    removed touch end

--- js/ie_app.js
+++ js/ie_app.js
@@ -63,11 +63,4 @@ $(document).ready(function () {
             e.preventDefault();
         });
     }
-
-    for (i = 0; i < portfolio.length; i += 1) {
-        portfolio[i].addEventListener('touchend', function () {
-            this.getElementsByTagName('h2')[0].style.color = "cadetblue";
-            this.getElementsByTagName('h3')[0].style.color = "darkslateblue";
-        });
-    }
 });
