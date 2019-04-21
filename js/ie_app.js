da887465aabc7aea75a7fbb16b9470f9691feca7
james-braun <braun.james.robert@gmail.com>
2019-04-20 22:26:36 -0500

    seperated touches

--- js/ie_app.js
+++ js/ie_app.js
@@ -59,10 +59,18 @@ $(document).ready(function () {
     }
 
     for (i = 0; i < portfolio.length; i += 1) {
-        portfolio[i].addEventListener('touchstart touchend', function () {
+        portfolio[i].addEventListener('touchstart', function () {
             this.preventDefault();
             this.getElementsByTagName('h2')[0].style.color = "darkslateblue";
             this.getElementsByTagName('h3')[0].style.color = "cadetblue";
+        });
+    }
+
+    for (i = 0; i < portfolio.length; i += 1) {
+        portfolio[i].addEventListener('touchend', function () {
+            this.preventDefault();
+            this.getElementsByTagName('h2')[0].style.color = "cadetblue";
+            this.getElementsByTagName('h3')[0].style.color = "darkslateblue";
         });
     }
 });
