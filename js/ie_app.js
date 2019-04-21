f69a3da46d5eb92a7b81aa39036141fc2ad0b25c
james-braun <braun.james.robert@gmail.com>
2019-04-20 22:32:04 -0500

    remove e

--- js/ie_app.js
+++ js/ie_app.js
@@ -68,7 +68,6 @@ $(document).ready(function () {
 
     for (i = 0; i < portfolio.length; i += 1) {
         portfolio[i].addEventListener('touchend', function (e) {
-            e.preventDefault();
             this.getElementsByTagName('h2')[0].style.color = "cadetblue";
             this.getElementsByTagName('h3')[0].style.color = "darkslateblue";
         });
