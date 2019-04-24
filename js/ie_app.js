8ab9eafd8ee4d6acf002eccb931fca1b9f0acfb4
james-braun <braun.james.robert@gmail.com>
2019-04-24 18:23:56 -0500

    added onselectstatt

--- js/ie_app.js
+++ js/ie_app.js
@@ -94,6 +94,7 @@ $(document).ready(function () {
 
         // change colors on touchstart and lift portfolio item off the page.
         portfolio[i].addEventListener('touchstart', function () {
+            this.onselectstart = function () { return false; };
             this.getElementsByTagName('h2')[0].style.color = "white";
             this.getElementsByTagName('h3')[0].style.color = "antiquewhite";
             this.style.zIndex = 1000;
