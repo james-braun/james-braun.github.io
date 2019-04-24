030a143c51a262d3ba3a5ad6981c4eb3587fb9a1
james-braun <braun.james.robert@gmail.com>
2019-04-24 16:36:50 -0500

    removed immediate

--- js/ie_app.js
+++ js/ie_app.js
@@ -67,7 +67,7 @@ $(document).ready(function () {
         // if repository touchstart then goto repository page.
         repository[i].addEventListener('touchstart', function (e) {
             e.preventDefault();
-            e.stopImmediatePropagation();
+            e.stopPropagation();
             window.location.href = this.href;
         });
 
