17f010c27780de89f91a6544376ab2008efdffb2
james-braun <braun.james.robert@gmail.com>
2019-04-24 23:58:33 -0500

    removed webkittransform

--- js/ie_app.js
+++ js/ie_app.js
@@ -120,7 +120,6 @@ $(document).ready(function () {
 
             // put item back on page.
             this.style.transform = "scale(1)";
-            this.style.webkitTransform = "scale(1)";
             this.style.zIndex = 0;
 
             // after 750 mili-second if user has not scrolled then goto project page.
